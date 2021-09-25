67e304e2bb331a7e59e05f797b61bf43a9372b93
MSF SaznyRahnas <88286372+RahnasMSFS@users.noreply.github.com>
2021-09-25 10:30:48 -0700

    Add files via upload

---
+++ ManyToManyRelApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.vau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyRelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyToManyRelApplication.class, args);
+	}
+
+}
